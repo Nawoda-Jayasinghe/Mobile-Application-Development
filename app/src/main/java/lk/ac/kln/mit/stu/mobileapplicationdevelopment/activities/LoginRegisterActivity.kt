ba88fd09c27023966689166abc07c160de7d4d68
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2023-01-29 11:28:44 +0530

    Setup firebase and the folder structure. Defined fonts and colors

---
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/LoginRegisterActivity.kt
@@ -0,0 +1,13 @@
+package lk.ac.kln.mit.stu.mobileapplicationdevelopment.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+    //check git branch
+}
---
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package lk.ac.kln.mit.stu.mobileapplicationdevelopment.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
