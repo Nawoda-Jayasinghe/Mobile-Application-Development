d98ac9ff98d97e00ebebbb8f358bab7805bf15a8
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2023-02-02 17:25:14 +0530

    fixed the navigation issue on view binding

--- app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/ShoppingActivity.kt
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/ShoppingActivity.kt
@@ -5,22 +5,29 @@ import android.os.Bundle
 import android.view.View.inflate
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
-
-
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityShoppingBinding
 
-
-
+//    val binding by lazy{
+//        ActivityShoppingBinding.inflate(layoutInflater)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
-
+//        setContentView(R.layout.activity_shopping)
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
+
+//        reffer to https://www.geeksforgeeks.org/view-binding-with-fragments-in-android-jetpack/ of get back any error in futre
+
     }
 }
 
