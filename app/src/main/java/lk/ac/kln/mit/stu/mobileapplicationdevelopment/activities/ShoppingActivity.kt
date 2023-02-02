d7f60bcf6aaf99c453a52b9de2c8564d2ae7d096
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2023-02-02 16:41:33 +0530

    added the bottom navbar and connected the main screen with shopping screen

--- app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/ShoppingActivity.kt
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/ShoppingActivity.kt
@@ -2,11 +2,26 @@ package lk.ac.kln.mit.stu.mobileapplicationdevelopment.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
 
+
+
 class ShoppingActivity : AppCompatActivity() {
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+
+        val navController = findNavController(R.id.shoppingHostFragment)
     }
 }
+
+
---
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,7 @@
+package lk.ac.kln.mit.stu.mobileapplicationdevelopment.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
