72d20209145c79bf0855184bcf6aa438bb64c6e0
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2023-02-03 09:41:02 +0530

    Added the tablayout of home screen

---
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/fragments/categories/BaseCategoryFragment.kt
@@ -0,0 +1,9 @@
+package lk.ac.kln.mit.stu.mobileapplicationdevelopment.fragments.categories
+
+import androidx.fragment.app.Fragment
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
+
+//have to add open as this class is extendable
+
+open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
+}
--- app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/fragments/shopping/HomeFragment.kt
@@ -1,7 +1,51 @@
 package lk.ac.kln.mit.stu.mobileapplicationdevelopment.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.adapter.HomeViewpagerAdapter
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.databinding.FragmentHomeBinding
+import lk.ac.kln.mit.stu.mobileapplicationdevelopment.fragments.categories.*
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val categoriesFragments = arrayListOf<Fragment>(
+            MainCategoryFragment(),
+            BedroomFragment(),
+            BathroomFragment(),
+            DiningroomFragment(),
+            KitchenroomFragment(),
+            LivingroomFragment()
+        )
+
+        val viewpagerAdapter = HomeViewpagerAdapter(categoriesFragments, childFragmentManager,lifecycle)
+        binding.viewpagerHome.adapter = viewpagerAdapter
+        TabLayoutMediator(binding.tabLayout,binding.viewpagerHome){ tab, position ->
+            when(position){
+                0 -> tab.text = "All Products"
+                1 -> tab.text = "Bedroom Products"
+                2 -> tab.text = "Bathroom Products"
+                3 -> tab.text = "Diningroom Products"
+                4 -> tab.text = "Kitchenroom Products"
+                5 -> tab.text = "Livingroom Products"
+            }
+        }.attach()
+    }
 }
