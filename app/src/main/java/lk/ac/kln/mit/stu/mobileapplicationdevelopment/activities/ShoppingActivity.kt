8fea70b08550ef95084961d1b81f9d994586b076
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2023-02-05 22:11:35 +0530

    Commited with run time errors

--- app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/ShoppingActivity.kt
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/ShoppingActivity.kt
@@ -6,29 +6,25 @@ import android.view.View.inflate
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
 import lk.ac.kln.mit.stu.mobileapplicationdevelopment.databinding.ActivityShoppingBinding
 
+@AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityShoppingBinding
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
 
-//    val binding by lazy{
-//        ActivityShoppingBinding.inflate(layoutInflater)
-//    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_shopping)
-        binding = ActivityShoppingBinding.inflate(layoutInflater)
+    override fun onCreate(savedInstancesState: Bundle?) {
+        super.onCreate(savedInstancesState)
         setContentView(binding.root)
 
         val navController = findNavController(R.id.shoppingHostFragment)
         binding.bottomNavigation.setupWithNavController(navController)
-
-//        reffer to https://www.geeksforgeeks.org/view-binding-with-fragments-in-android-jetpack/ of get back any error in futre
-
     }
+
 }
 
 
