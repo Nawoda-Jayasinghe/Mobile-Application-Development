37b761b1c03a54c9f02129739415efa513b6e8ec
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2023-02-10 23:07:09 +0530

    created email accounts and connected with firebase

--- app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/LoginRegisterActivity.kt
+++ app/src/main/java/lk/ac/kln/mit/stu/mobileapplicationdevelopment/activities/LoginRegisterActivity.kt
@@ -4,26 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import dagger.hilt.android.AndroidEntryPoint
 import lk.ac.kln.mit.stu.mobileapplicationdevelopment.R
 
+@AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_introduction)
+        setContentView(R.layout.activity_login_register)
 
-        val getStart = findViewById<Button>(R.id.btnGetStart)
-        getStart.setOnClickListener{
-            //we use intend to navigate form one activity to another
-            // we have to give 2 arguments here. (current view, the pace we want to naigated)
-            val intent = Intent(this, ShoppingActivity ::class.java) // this is an intent object
 
-            //intent is a class in android libraries. here we have created an object from the class
-            //mostly in android we create objects from andoird classes and invoke the functions.
-            //but sometimes we have to create own classes and objects
-            startActivity(intent) //now we navigate from one activity to another
         }
     }
-
-
-    //check git branch 2
-}
